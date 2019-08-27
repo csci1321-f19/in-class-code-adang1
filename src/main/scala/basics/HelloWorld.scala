3672a3eed49820e67ef0c36ad96db7217019a84c
Anh Dang <adang1@trinity.edu>
2019-08-27 11:00:50 -0500

    Added entity with intersects.

--- src/main/scala/basics/HelloWorld.scala
+++ src/main/scala/basics/HelloWorld.scala
@@ -1,11 +1,17 @@
 package basics
 
+import graphicgame.Entity
+import org.w3c.dom.Entity
 /**
 This is a basic main for you to start off with.
 */
 object HelloWorld {
 	def main(args: Array[String]): Unit = {
 		println("Hello Vau!")
+
+		val e1 = new Entity(2,4)
+		val e2 = new Entity(1,3)
+		println(e1.intersects(e2))
 	}
 	
 	def square(x: Double) = x*x
